33a0754dc3457a6768c08b33155ccd31b7d10acb
Jan Christophersen <jan.christophersen@digitalunit.berlin>
2018-08-14 07:57:49 +0200

    Vue rendering of members

--- lib/gatekeeper/teams/team.ex
+++ lib/gatekeeper/teams/team.ex
@@ -2,11 +2,10 @@ defmodule Gatekeeper.Teams.Team do
   use Ecto.Schema
   import Ecto.Changeset
 
-
   schema "teams" do
-    field :name, :string
+    field(:name, :string)
 
-    many_to_many :members, Gatekeeper.Users.User, join_through: "team_members"
+    many_to_many(:members, Gatekeeper.Users.User, join_through: "team_members")
     timestamps()
   end
 
@@ -15,5 +14,13 @@ defmodule Gatekeeper.Teams.Team do
     team
     |> cast(attrs, [:name])
     |> validate_required([:name])
+  end
+
+  def safe_json(team) do
+    %{
+      id: team.id,
+      name: team.name,
+      members: Enum.map(team.members, fn m -> Gatekeeper.Users.User.safe_json(m) end)
+    }
   end
 end

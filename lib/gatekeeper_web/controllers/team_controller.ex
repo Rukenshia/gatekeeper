1d3544833f5822699f22d459fca6c83276cfdda8
Jan Christophersen <jan@ruken.pw>
2018-09-13 13:42:31 -0500

    Remove team functionality Closes #11, Closes #12

--- lib/gatekeeper_web/controllers/team_controller.ex
+++ lib/gatekeeper_web/controllers/team_controller.ex
@@ -7,16 +7,6 @@ defmodule GatekeeperWeb.TeamController do
   alias Gatekeeper.Teams
   alias Gatekeeper.Teams.Team
 
-  def index(conn, _params) do
-    teams = Teams.list_teams()
-    render(conn, "index.html", teams: teams)
-  end
-
-  def new(conn, _params) do
-    changeset = Teams.change_team(%Team{})
-    render(conn, "new.html", changeset: changeset)
-  end
-
   def create(conn, %{"team" => team_params}) do
     case Teams.create_team(team_params) do
       {:ok, team} ->
@@ -41,39 +31,9 @@ defmodule GatekeeperWeb.TeamController do
   def edit(conn, %{"id" => id}) do
     team = Teams.get_team!(id)
 
-    changeset = Teams.change_team(team)
-
     render(conn, "edit.html",
       team: team,
-      changeset: changeset,
       vue_data: Poison.encode!(%{team: Team.safe_json(team)}, pretty: true)
     )
-  end
-
-  def update(conn, %{"id" => id, "team" => team_params}) do
-    team = Teams.get_team!(id)
-
-    case Teams.update_team(team, team_params) do
-      {:ok, team} ->
-        conn
-        |> put_flash(:info, "Team updated successfully.")
-        |> redirect(to: team_path(conn, :show, team))
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "edit.html",
-          team: team,
-          changeset: changeset,
-          vue_data: Poison.encode!(%{team: Team.safe_json(team)}, pretty: true)
-        )
-    end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    team = Teams.get_team!(id)
-    {:ok, _team} = Teams.delete_team(team)
-
-    conn
-    |> put_flash(:info, "Team deleted successfully.")
-    |> redirect(to: team_path(conn, :index))
   end
 end
